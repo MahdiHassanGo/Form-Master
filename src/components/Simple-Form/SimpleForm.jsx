10530f1c9475cec7836de059f13176164b551fc2
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-10-31 00:11:36 +0600

    pushing handlers

--- src/components/Simple-Form/SimpleForm.jsx
+++ src/components/Simple-Form/SimpleForm.jsx
@@ -1,15 +1,35 @@
-const SimpleForm = () => {
+import { useState } from "react";
+
+const StatefulForm = () => {
+
+
+    const [email,setEmail]= useState(null);
+    const [password,setPassword] =useState(null);
+
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
   };
+  const handleEmailChange = e =>{
+
+     setEmail(e.target.value);
+  }
+  const handlePassChange = e =>{
+    setPassword(e.target.value)
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" value="" />
+        <input type="text" name="name" value="" id=""/>
         <br />
-        <input type="email" name="email" value="" />
+        <input onChange={handleEmailChange}
+         type="email" name="email" id="" />
+        <br />
+        <input onChange={handlePassChange}
+         type="password" name="password" />
         <br />
         <input type="submit" value="submit" />
       </form>
@@ -17,4 +37,4 @@ const SimpleForm = () => {
   );
 };
 
-export default SimpleForm;
+export default StatefulForm;
