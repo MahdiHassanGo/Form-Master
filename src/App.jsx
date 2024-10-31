e9d34cfce8515545071b418041366693d799b06c
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-10-31 14:38:08 +0600

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-
 import './App.css'
-import RefForm from './components/RefForm/RefForm'
-// import StatefulForm from './components/Simple-Form/SimpleForm'
-// import SimpleForm from './components/Simple-Form/SimpleForm'
+import Grandpa from './components/Grandpa/Grandpa'
+// import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
+// import RefForm from './components/RefForm/RefForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
-  
+
+  // const handleSingUpSubmit = data => {
+  //   console.log('sign up data', data);
+  // }
+
+  // const handleUpdateProfile = data => {
+  //   console.log('update profile', data);
+  // }
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
+
+      <Grandpa></Grandpa>
+
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
-      <RefForm></RefForm>
-   
+      {/* <RefForm></RefForm> */}
+      {/* <HookForm></HookForm> */}
+      {/* <ReusableForm
+        formTitle={'Sign UP'}
+        handleSubmit={handleSingUpSubmit}>
+          <div>
+            <h2>Sing UP</h2>
+            <p>please sign up right now</p>
+          </div>
+        </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+          <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReusableForm> */}
     </>
   )
 }
